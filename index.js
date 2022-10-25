fabde4867a4506cf247d7d15ece505edf6159587
obaedulislam <obaed.cse@gmail.com>
2022-10-25 19:09:13 +0600

    Add JSON Data & Showed them localhost succesfully

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/courses.json');
+
+app.get("/", (req, res) => {
+    res.send("Now Server is Running");
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course.id === id);
+    res.send(selectedCourse);
+})
+
 
 //Test server running port 
 app.listen(port, () => {
