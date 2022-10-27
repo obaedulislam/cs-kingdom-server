eecfb03b053ae6731ff9e56b720ba1071f4bdd23
obaedulislam <obaed.cse@gmail.com>
2022-10-27 06:22:20 +0600

    Vercel Setup Done

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4100;
 
 const courses = require('./data/courses.json');
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.send("Now Server is Running");
 })
 
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
@@ -19,6 +22,7 @@ app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
     const selectedCourse = courses.find(c => c.id = id);
     res.send(selectedCourse);
+    console.log(selectedCourse);
 })
 
 
