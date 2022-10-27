28cb0c48888a6078b434e36616f4ef5d39edb9e4
obaedulislam <obaed.cse@gmail.com>
2022-10-27 15:39:06 +0600

    Some JSON info Added

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-app.use(cors());
 
 const port = process.env.PORT || 4100;
+app.use(cors());
+
 
 const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
-    res.send("Now Server is Running");
+    res.send("Now Server is Running Again");
 })
 
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
-app.get('/courses', (req, res) => {
-    res.send(courses);
-})
 
-app.get('/courses/:id', (req, res) => {
+
+app.get('/course/:id', (req, res) => {
     const id = req.params.id;
-    const selectedCourse = courses.find(c => c.id = id);
+    const selectedCourse = courses.find(c => c.id === id);
     res.send(selectedCourse);
     console.log(selectedCourse);
 })
